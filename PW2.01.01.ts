f6e9a0d245984aa681e97a043aef0cbd8c1445cf
Ekramul <mdekramulhassan168@gmail.com>
2025-02-08 11:47:50 +0600

    searchInsert problem  added

--- PW2.01.01.ts
+++ PW2.01.01.ts
@@ -36,3 +36,29 @@ console.log(removeDuplicates(nums1), nums1); // Output: 2, nums = [1,2,_]
 
 let nums2 = [0, 0, 1, 1, 1, 2, 2, 3, 3, 4];
 console.log(removeDuplicates(nums2), nums2); // Output: 5, nums = [0,1,2,3,4,_]
+
+// searchInsert
+
+function searchInsert(nums: number[], target: number): number {
+  let left = 0,
+    right = nums.length - 1;
+
+  while (left <= right) {
+    let mid = Math.floor((left + right) / 2);
+
+    if (nums[mid] === target) {
+      return mid;
+    } else if (nums[mid] < target) {
+      left = mid + 1;
+    } else {
+      right = mid - 1;
+    }
+  }
+
+  return left;
+}
+
+// Example usage:
+console.log(searchInsert([1, 3, 5, 6], 5)); // Output: 2
+console.log(searchInsert([1, 3, 5, 6], 2)); // Output: 1
+console.log(searchInsert([1, 3, 5, 6], 7)); // Output: 4
