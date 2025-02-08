8969aa0c5749b59b426cf70fe9dec5bc224185bf
Ekramul <mdekramulhassan168@gmail.com>
2025-02-08 11:44:42 +0600

    pw2 code added

--- PW2.01.01.ts
+++ PW2.01.01.ts
@@ -15,3 +15,24 @@ function twoSum(nums: number[], target: number): number[] {
 console.log(twoSum([2, 7, 11, 15], 9));
 console.log(twoSum([3, 2, 4], 6));
 console.log(twoSum([3, 3], 6));
+
+// removeDuplicates
+function removeDuplicates(nums: number[]): number {
+  if (nums.length === 0) return 0;
+  let k = 1;
+
+  for (let i = 1; i < nums.length; i++) {
+    if (nums[i] !== nums[i - 1]) {
+      nums[k] = nums[i];
+      k++;
+    }
+  }
+  return k;
+}
+
+// Example usage
+let nums1 = [1, 1, 2];
+console.log(removeDuplicates(nums1), nums1); // Output: 2, nums = [1,2,_]
+
+let nums2 = [0, 0, 1, 1, 1, 2, 2, 3, 3, 4];
+console.log(removeDuplicates(nums2), nums2); // Output: 5, nums = [0,1,2,3,4,_]
