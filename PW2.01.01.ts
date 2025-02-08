c0f437eab7f6ae776446a4c10ac17814e2d05054
Ekramul <mdekramulhassan168@gmail.com>
2025-02-08 11:49:23 +0600

    nextPermutation problem added

--- PW2.01.01.ts
+++ PW2.01.01.ts
@@ -62,3 +62,42 @@ function searchInsert(nums: number[], target: number): number {
 console.log(searchInsert([1, 3, 5, 6], 5)); // Output: 2
 console.log(searchInsert([1, 3, 5, 6], 2)); // Output: 1
 console.log(searchInsert([1, 3, 5, 6], 7)); // Output: 4
+
+// nextPermutation
+
+function nextPermutation(nums: number[]): void {
+  let i = nums.length - 2;
+
+  while (i >= 0 && nums[i] >= nums[i + 1]) {
+    i--;
+  }
+
+  if (i >= 0) {
+    let j = nums.length - 1;
+    while (nums[j] <= nums[i]) {
+      j--;
+    }
+    [nums[i], nums[j]] = [nums[j], nums[i]];
+  }
+
+  let left = i + 1,
+    right = nums.length - 1;
+  while (left < right) {
+    [nums[left], nums[right]] = [nums[right], nums[left]];
+    left++;
+    right--;
+  }
+}
+
+// Example usage:
+let nums3 = [1, 2, 3];
+nextPermutation(nums3);
+console.log(nums3); // Output: [1,3,2]
+
+let nums4 = [3, 2, 1];
+nextPermutation(nums4);
+console.log(nums4); // Output: [1,2,3]
+
+let nums5 = [1, 1, 5];
+nextPermutation(nums5);
+console.log(nums5); // Output: [1,5,1]
