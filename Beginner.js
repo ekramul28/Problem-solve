c49b2cd3b024fdafa908286aafa9463bbf362adb
Ekramul <mdekramulhassan168@gmail.com>
2025-02-14 20:04:35 +0600

    beginner2 problem added

--- Beginner.js
+++ Beginner.js
@@ -4,3 +4,16 @@ const reverseString = (str) => {
   return str.split("").reverse().join("");
 };
 console.log(reverseString("hello"));
+
+// Write a function that checks if a given string is a palindrome (reads the same backward and forward).
+
+function isPalindrome(str) {
+  let reversed = str.split("").reverse().join("");
+  return str === reversed;
+}
+console.log(isPalindrome("racecar"));
+
+const isPalindrome = (str) => {
+  let reversed = str.split("").reverse().join();
+  return reversed === str;
+};
