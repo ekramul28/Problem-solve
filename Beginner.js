7027fc819c0abf76029808abc805b7d3c3ffa6e5
Ekramul <mdekramulhassan168@gmail.com>
2025-02-14 20:25:14 +0600

    add new problem

--- Beginner.js
+++ Beginner.js
@@ -7,13 +7,15 @@ console.log(reverseString("hello"));
 
 // Write a function that checks if a given string is a palindrome (reads the same backward and forward).
 
-function isPalindrome(str) {
-  let reversed = str.split("").reverse().join("");
-  return str === reversed;
-}
-console.log(isPalindrome("racecar"));
-
 const isPalindrome = (str) => {
   let reversed = str.split("").reverse().join();
   return reversed === str;
 };
+
+// Write a function that takes an array of numbers and returns the largest number
+
+const largestNumberOfArray = (array) => {
+  return Math.max(...array);
+};
+
+console.log(largestNumberOfArray([2, 5, 767, 432, 2455]));
