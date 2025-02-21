60d7104b8571250d9710184c91f12e1f83ba43a4
Ekramul <mdekramulhassan168@gmail.com>
2025-02-21 19:22:53 +0600

    Roman to Integer

--- PW2.01.01.ts
+++ PW2.01.01.ts
@@ -138,3 +138,25 @@ function intToRoman(num) {
 console.log(intToRoman(3));
 console.log(intToRoman(58));
 console.log(intToRoman(1994));
+
+// PW2.01.08 Roman to Integer
+
+function romanToInt(s) {
+  const romanMap = { I: 1, V: 5, X: 10, L: 50, C: 100, D: 500, M: 1000 };
+  let total = 0;
+
+  for (let i = 0; i < s.length; i++) {
+    if (i < s.length - 1 && romanMap[s[i]] < romanMap[s[i + 1]]) {
+      total -= romanMap[s[i]];
+    } else {
+      total += romanMap[s[i]];
+    }
+  }
+
+  return total;
+}
+
+// Test cases
+console.log(romanToInt("III"));
+console.log(romanToInt("LVIII"));
+console.log(romanToInt("MCMXCIV"));
