a87c5276f7799bd5217d1c93d512cacdbe535875
Ekramul <mdekramulhassan168@gmail.com>
2025-03-03 13:39:55 +0600

    Concatenation array added problem 2 added

--- PW2.01.06.ts
+++ PW2.01.06.ts
@@ -7,3 +7,13 @@ function buildArray(nums: number[]): number[] {
 // Example usage:
 console.log(buildArray([0, 2, 1, 5, 3, 4])); // Output: [0,1,2,4,5,3]
 console.log(buildArray([5, 0, 1, 2, 3, 4])); // Output: [4,5,0,1,2,3]
+
+// Concatenation of Array
+
+function getConcatenation(nums: number[]): number[] {
+  return [...nums, ...nums];
+}
+
+// Example usage:
+console.log(getConcatenation([1, 2, 1])); // Output: [1,2,1,1,2,1]
+console.log(getConcatenation([1, 3, 2, 1])); // Output: [1,3,2,1,1,3,2,1]
