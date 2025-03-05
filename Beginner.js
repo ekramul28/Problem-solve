195b0460909b6f9f09e431a1c9a792be1de78d2f
Ekramul <mdekramulhassan168@gmail.com>
2025-03-05 22:05:37 +0600

    sleep problem added

--- Beginner.js
+++ Beginner.js
@@ -97,3 +97,9 @@ function findLargest(arr) {
   return Math.max(...arr);
 }
 console.log(findLargest([3, 7, 2, 8, 10]));
+
+function sleep(ms) {
+  // Your code here
+}
+
+sleep(2000).then(() => console.log("Waited for 2 seconds"));
