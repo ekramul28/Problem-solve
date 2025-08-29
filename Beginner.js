35f364d696a66d86db4c60d041ca4899bf9b16e9
Ekramul <mdekramulhassan168@gmail.com>
2025-08-29 10:36:48 +0600

    added lengthOfLongestSubstring

--- Beginner.js
+++ Beginner.js
@@ -122,3 +122,24 @@ function countVowels(str) {
   return [...str].filter((char) => vowels.includes(char)).length;
 }
 console.log(countVowels("Hello World")); // 3
+
+function lengthOfLongestSubstring(s) {
+  let set = new Set();
+  let left = 0,
+    maxLength = 0;
+
+  for (let right = 0; right < s.length; right++) {
+    while (set.has(s[right])) {
+      set.delete(s[left]);
+      left++;
+    }
+    set.add(s[right]);
+    maxLength = Math.max(maxLength, right - left + 1);
+  }
+
+  return maxLength;
+}
+
+console.log(lengthOfLongestSubstring("abcabcbb")); // 3
+console.log(lengthOfLongestSubstring("bbbbb")); // 1
+console.log(lengthOfLongestSubstring("pwwkew")); // 3
